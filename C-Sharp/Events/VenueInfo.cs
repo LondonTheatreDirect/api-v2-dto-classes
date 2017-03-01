1273e10109871e7c75cd6eac237d89e9f0500569
vojtechkozuch-LTD <vojtech.kozuch@ictcapital.cz>
2017-03-01 16:06:13 +0100

    Create VenueInfo.cs

---
+++ C-Sharp/Events/VenueInfo.cs
@@ -0,0 +1,19 @@
+namespace LTD_API
+{
+    public class VenueInfo
+    {
+        public int VenueId { get; set; }
+        public string Name { get; set; }
+        public string Info { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Postcode { get; set; }
+        public string Telephone { get; set; }
+        public string Fax { get; set; }
+        public string Email { get; set; }
+        public string NearestTube { get; set; }
+        public string Train { get; set; }
+        public string PlanLink { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
