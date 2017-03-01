41260a8a1cff130d3834ce1b86917ea5a4362ba5
vojtechkozuch-LTD <vojtech.kozuch@ictcapital.cz>
2017-03-01 16:12:25 +0100

    Create Country.cs

---
+++ C-Sharp/Miscellaneous/Country.cs
@@ -0,0 +1,10 @@
+namespace LTD_API
+{
+    public class Country
+    {
+        public int CountryId { get; set; }
+        public string CountryName { get; set; }
+        public int Ordering { get; set; }
+        public string ISO3166 { get; set; }
+    }
+}
