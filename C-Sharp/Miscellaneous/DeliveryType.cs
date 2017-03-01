988f14882e8101b56bbe39a692a6d0e91ae56ef2
vojtechkozuch-LTD <vojtech.kozuch@ictcapital.cz>
2017-03-01 16:12:51 +0100

    Create DeliveryType.cs

---
+++ C-Sharp/Miscellaneous/DeliveryType.cs
@@ -0,0 +1,9 @@
+namespace LTD_API
+{
+    public class DeliveryType
+    {
+        public int DeliveryTypeId { get; set; }
+        public string DeliveryTypeName { get; set; }
+        public decimal DeliveryTypePrice { get; set; }
+    }
+}
