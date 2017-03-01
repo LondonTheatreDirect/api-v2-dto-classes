4ca04167c9edab417b7c5a24a556438a788bbb05
vojtechkozuch-LTD <vojtech.kozuch@ictcapital.cz>
2017-03-01 16:03:41 +0100

    Create Performance.cs

---
+++ C-Sharp/Events/Performance.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LTD_API
+{
+    public class Performance
+    {
+        public int PerformanceId { get; set; }
+        public DateTime PerformanceDate { get; set; }
+        public bool IsUnreserved { get; set; }
+    }
+}
