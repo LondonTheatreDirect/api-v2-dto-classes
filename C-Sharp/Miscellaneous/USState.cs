bc07e8666af8044c1a83b323698878a28381a95f
vojtechkozuch-LTD <vojtech.kozuch@ictcapital.cz>
2017-03-01 16:17:44 +0100

    Create USState.cs

---
+++ C-Sharp/Miscellaneous/USState.cs
@@ -0,0 +1,9 @@
+namespace LTD_API
+{
+    public class USState
+    {
+        public int StateId { get; set; }
+        public string StateName { get; set; }
+        public string StateCode { get; set; }
+    }
+}
