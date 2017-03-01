c486c84fc9c8ce1281a24e8080f7177fbd09643d
vojtechkozuch-LTD <vojtech.kozuch@ictcapital.cz>
2017-03-01 16:03:19 +0100

    Create Image.cs

---
+++ C-Sharp/Events/Image.cs
@@ -0,0 +1,9 @@
+namespace LTD_API
+{
+    public class Image
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Url { get; set; }
+    }
+}
