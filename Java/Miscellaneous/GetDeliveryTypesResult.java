914b73a43406de947b861c561603f45a7beffdaf
vojtechkozuch-LTD <vojtech.kozuch@ictcapital.cz>
2017-03-07 13:05:40 +0100

    Create GetDeliveryTypesResult.java

---
+++ Java/Miscellaneous/GetDeliveryTypesResult.java
@@ -0,0 +1,18 @@
+package LTD_API;
+
+import java.util.List;
+
+public class GetDeliveryTypesResult
+{
+    private List<DeliveryType> deliveryTypes;
+    
+    public List<DeliveryType> getDeliveryTypes()
+    {
+        return deliveryTypes;
+    }
+    public void setDeliveryTypes(List<DeliveryType> deliveryTypes)
+    {
+        this.deliveryTypes = deliveryTypes;
+    }
+}
+
