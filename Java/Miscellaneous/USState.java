6ae71bbb79257c884af3ac00e8cd250b8880a1d3
vojtechkozuch-LTD <vojtech.kozuch@ictcapital.cz>
2017-03-07 13:08:37 +0100

    Create USState.java

---
+++ Java/Miscellaneous/USState.java
@@ -0,0 +1,36 @@
+package LTD_API;
+
+public class USState
+{
+    private int stateId;
+    private String stateName;
+    private String stateCode;
+    
+    public int getStateId()
+    {
+        return stateId;
+    }
+    public void setStateId(int stateId)
+    {
+        this.stateId = stateId;
+    }
+
+    public String getStateName()
+    {
+        return stateName;
+    }
+    public void setStateName(String stateName)
+    {
+        this.stateName = stateName;
+    }
+
+    public String getStateCode()
+    {
+        return stateCode;
+    }
+    public void setStateCode(String stateCode)
+    {
+        this.stateCode = stateCode;
+    }
+}
+
