548ae128a33889d12aa2f1d9b0851c8a8a2079ae
vojtechkozuch-LTD <vojtech.kozuch@ictcapital.cz>
2017-03-07 13:00:47 +0100

    Create GetTicketPlanPriceResult.java

---
+++ Java/Booking/GetTicketPlanPriceResult.java
@@ -0,0 +1,16 @@
+package LTD_API;
+
+public class GetTicketPlanPriceResult
+{
+    private decimal price;
+    
+    public decimal getPrice()
+    {
+        return price;
+    }
+    public void setPrice(decimal price)
+    {
+        this.price = price;
+    }
+}
+
