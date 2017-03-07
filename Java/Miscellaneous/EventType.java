18665019aa9db9e00252cdd530c85940022e4f90
vojtechkozuch-LTD <vojtech.kozuch@ictcapital.cz>
2017-03-07 13:04:53 +0100

    Create EventType.java

---
+++ Java/Miscellaneous/EventType.java
@@ -0,0 +1,26 @@
+package LTD_API;
+
+public class EventType
+{
+    private int eventTypeId;
+    private String eventTypeName;
+    
+    public int getEventTypeId()
+    {
+        return eventTypeId;
+    }
+    public void setEventTypeId(int eventTypeId)
+    {
+        this.eventTypeId = eventTypeId;
+    }
+
+    public String getEventTypeName()
+    {
+        return eventTypeName;
+    }
+    public void setEventTypeName(String eventTypeName)
+    {
+        this.eventTypeName = eventTypeName;
+    }
+}
+
