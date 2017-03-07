1dccaf6a8548789bc0a4f048d9ef8824d914ba5a
vojtechkozuch-LTD <vojtech.kozuch@ictcapital.cz>
2017-03-07 13:07:00 +0100

    Create GetUSStatesResult.java

---
+++ Java/Miscellaneous/GetUSStatesResult.java
@@ -0,0 +1,18 @@
+package LTD_API;
+
+import java.util.List;
+
+public class GetUSStatesResult
+{
+    private List<USState> usStates;
+    
+    public List<USState> getUSStates()
+    {
+        return usStates;
+    }
+    public void setUSStates(List<USState> usStates)
+    {
+        this.usStates = usStates;
+    }
+}
+
