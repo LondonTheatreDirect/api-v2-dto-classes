731914bbb3da990e0d1089a4bf9f2c6f505f82c3
vojtechkozuch-LTD <vojtech.kozuch@ictcapital.cz>
2017-07-18 09:26:00 +0200

    Update FullTicketInfo.cs

--- C-Sharp/Events/FullTicketInfo.cs
+++ C-Sharp/Events/FullTicketInfo.cs
@@ -1,21 +1,22 @@
-package LTD_API;
-
-public class FullTicketInfo
+namespace LTD_API
 {
-        [DataMember]
-        public int TicketId { get; set; }
-        [DataMember]
-        public string AreaName { get; set; }
-        [DataMember]
-        public string TicketName1 { get; set; }
-        [DataMember]
-        public string TicketName2 { get; set; }
-        [DataMember]
-        public decimal SellingPrice { get; set; }
-        [DataMember]
-        public decimal FaceValue { get; set; }
-        [DataMember]
-        public bool RestrictedView { get; set; }
-        [DataMember]
-        public string Description { get; set; }
+        public class FullTicketInfo
+        {
+                [DataMember]
+                public int TicketId { get; set; }
+                [DataMember]
+                public string AreaName { get; set; }
+                [DataMember]
+                public string TicketName1 { get; set; }
+                [DataMember]
+                public string TicketName2 { get; set; }
+                [DataMember]
+                public decimal SellingPrice { get; set; }
+                [DataMember]
+                public decimal FaceValue { get; set; }
+                [DataMember]
+                public bool RestrictedView { get; set; }
+                [DataMember]
+                public string Description { get; set; }
+        }
 }
