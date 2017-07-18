f1cbed508f6d4bbf64ac4787c6f6b7f825ad2003
vojtechkozuch-LTD <vojtech.kozuch@ictcapital.cz>
2017-07-18 08:53:05 +0200

    Add files via upload

---
+++ Java/Events/GetFullPerformanceAvailabilityResult.java
@@ -0,0 +1,37 @@
+package LTD_API;
+
+import java.util.List;
+
+public class GetFullPerformanceAvailabilityResult
+{
+    private int performanceId;
+	private int eventId;
+	private List<FullTicketInfo> availableTickets;
+	
+	public int getPerformanceId()
+    {
+        return performanceId;
+    }
+    public void setPerformanceId(int performanceId)
+    {
+        this.performanceId = performanceId;
+    }
+
+	public int getEventId()
+    {
+        return eventId;
+    }
+    public void setEventId(int eventId)
+    {
+        this.eventId = eventId;
+    }
+
+	public List<FullTicketInfo> getAvailableTickets()
+    {
+        return availableTickets;
+    }
+    public void setAvailableTickets(List<FullTicketInfo> availableTickets)
+    {
+        this.availableTickets = availableTickets;
+    }
+}
