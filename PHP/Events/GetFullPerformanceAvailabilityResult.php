77bf07838dfdad4c658aa463cbe3e2620e1a3aed
vojtechkozuch-LTD <vojtech.kozuch@ictcapital.cz>
2017-07-18 08:53:29 +0200

    Add files via upload

---
+++ PHP/Events/GetFullPerformanceAvailabilityResult.php
@@ -0,0 +1,39 @@
+<?php
+namespace LTD_API;
+
+class GetFullPerformanceAvailabilityResult
+{
+    private $performanceId;
+	private $eventId;
+	//array of FullTicketInfo
+	private $availableTickets;
+	
+	public function getPerformanceId()
+    {
+        return $this->performanceId;
+    }
+    public function setPerformanceId($performanceId)
+    {
+        $this->performanceId = $performanceId;
+    }
+
+	public function getEventId()
+    {
+        return $this->eventId;
+    }
+    public function setEventId($eventId)
+    {
+        $this->eventId = $eventId;
+    }
+
+	public function getAvailableTickets()
+    {
+        return $this->availableTickets;
+    }
+    public function setAvailableTickets($availableTickets)
+    {
+        $this->availableTickets = $availableTickets;
+    }
+
+
+}
