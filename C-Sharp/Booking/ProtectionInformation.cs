ce51b36e260202c242a68cf4a7a6c8945dec4b75
vojtechkozuch-LTD <vojtech.kozuch@ictcapital.cz>
2017-07-21 10:14:30 +0200

    Add files via upload

---
+++ C-Sharp/Booking/ProtectionInformation.cs
@@ -0,0 +1,8 @@
+namespace LTD_API
+{
+    public class ProtectionInformation
+    {
+        public int ProtectedTicketsCount { get; set; }
+        public decimal TotalProtectionPrice { get; set; }
+    }
+}
