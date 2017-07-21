fab3f15a935e57c87a8aa207483aa3e816e13c87
vojtechkozuch-LTD <vojtech.kozuch@ictcapital.cz>
2017-07-21 10:15:46 +0200

    Add files via upload

---
+++ PHP/Booking/ProtectionInformation.php
@@ -0,0 +1,26 @@
+<?php
+namespace LTD_API;
+
+class ProtectionInformation
+{
+	private $protectedTicketsCount;
+	private $totalProtectionPrice;
+	
+	public function getProtectedTicketsCount()
+    {
+        return $this->protectedTicketsCount;
+    }
+    public function setProtectedTicketsCount($protectedTicketsCount)
+    {
+        $this->protectedTicketsCount = $protectedTicketsCount;
+    }
+
+    public function getTotalProtectionPrice()
+    {
+        return $this->totalProtectionPrice;
+    }
+    public function setTotalProtectionPrice($totalProtectionPrice)
+    {
+        $this->totalProtectionPrice = $totalProtectionPrice;
+    }
+}
